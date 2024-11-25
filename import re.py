49df6ffe1f5c2b07396b5f6ef509cbdb2dd46eee
Omonire <omonire4@gmail.com>
2024-11-25 10:24:41 +0100

    Add files via upload

---
+++ import re.py
@@ -0,0 +1,12 @@
+import re
+
+
+
+test_string = "This is a txt string and we find a in the a string"
+
+print(re.find("a",test_string))
+
+#Assignment: GO online find any txtx file online and import it read it bring in a number of word
+#2. Read more about import re
+# READ ON TRY AND
+# print(EXCEPT)
